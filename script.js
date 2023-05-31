8fe7b5440045b97afbc4b64677003171266570fe
Louie787 <tigrovakyla11@gmail.com>
2023-05-31 13:37:43 +0300

    fixes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   const menuBtn = document.querySelector('.header__menu-btn');
   const nav = document.querySelector('.header__nav');
   const navCloseBtn = document.querySelector('.header__nav-close-btn');
   const mainTitle = document.querySelector('.main__title');
   const loginBtn = document.querySelector('.login-btn');
 
-  menuBtn.addEventListener('click', function() {
+  menuBtn.addEventListener('click', function () {
     nav.classList.add('open');
     mainTitle.classList.add('open');
     document.body.style.overflow = 'hidden';
@@ -15,13 +15,13 @@ window.addEventListener('DOMContentLoaded', function() {
     elementToScroll.scrollIntoView({ behavior: 'smooth', block: 'start' });
   });
 
-  navCloseBtn.addEventListener('click', function() {
+  navCloseBtn.addEventListener('click', function () {
     nav.classList.remove('open');
     mainTitle.classList.remove('open');
     document.body.style.overflow = 'auto'; // Разрешение прокрутки body
   });
 
-  loginBtn.addEventListener('click', function(event) {
+  loginBtn.addEventListener('click', function (event) {
     event.preventDefault(); // Предотвращение перехода по ссылке
 
     const loginSection = document.getElementById('login');
@@ -34,8 +34,8 @@ window.addEventListener('DOMContentLoaded', function() {
   });
 
   const navLinks = document.querySelectorAll('.header__nav .nav__list-link');
-  navLinks.forEach(function(link) {
-    link.addEventListener('click', function(event) {
+  navLinks.forEach(function (link) {
+    link.addEventListener('click', function (event) {
       event.preventDefault(); // Предотвращение перехода по ссылке
 
       const targetId = this.getAttribute('href').substring(1);
@@ -53,9 +53,9 @@ window.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   let loginButton = document.querySelector('.login-btn');
-  loginButton.addEventListener('click', function(event) {
+  loginButton.addEventListener('click', function (event) {
     event.preventDefault();
     window.scrollTo({
       top: document.documentElement.scrollHeight,
